2edcd3344e4ac37d2c44b14de799d02eccf5e907
Abigail Wiggins <rebtaylor14@gmail.com>
2019-10-07 02:24:40 -0500

    Empty string validation added

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Write your JavaScript code here!
 
+function init () {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoWeight = document.querySelector("input[name=cargoWeight]");
+
+      let inputs = [pilotName, copilotName, fuelLevel, cargoWeight];
+      for (let i = 0; i < inputs.length; i++) {
+         if (inputs[i].value === "") {
+            window.alert("Please fill out all fields!");
+            preventDefault(event);
+         }
+      }
+   });
+}
+
+window.onload = init
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
